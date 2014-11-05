935fa7e1cf015539a68ad0415a8aaf9aaa408b73
Hubert <hubertpuszklewicz@gmail.com>
2014-11-05 01:11:03 -0500

    module working

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import login
-from flask import Flask, render_template,request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import Connection
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        username = request.form['un']
+        password = request.form['pw']
+        login.addUser(username, password)
+        return render_template("login.html")
     return render_template("login.html")
 
 if __name__=='__main__':
